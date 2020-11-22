0118c60c2e7bb2ff2155f8c6c7ca19e7d7cdbb89
PabloSz <pablosaez1995@gmail.com>
2020-11-22 20:45:55 -0300

    improve tests for "writeGenerate"

--- packages/gqless-cli/test/writeGenerate.test.ts
+++ packages/gqless-cli/test/writeGenerate.test.ts
@@ -1,10 +1,10 @@
 import fs from 'fs';
+import path from 'path';
 import { createTestApp } from 'test-utils';
 import tmp from 'tmp-promise';
 
 import { writeGenerate } from '../src/writeGenerate';
 
-const { readFile } = fs.promises;
 const { server, isReady } = createTestApp({
   schema: `
     type Query {
@@ -24,11 +24,14 @@ beforeAll(async () => {
   await isReady;
 });
 
-test('works', async () => {
+test('generates code and writes existing file', async () => {
   const tempFile = await tmp.file();
 
-  const shouldBeIncluded = '// This should be included';
   try {
+    const shouldBeIncluded = '// This should be included';
+
+    const firstStats = await fs.promises.stat(tempFile.path);
+
     const destinationPath = await writeGenerate(
       server.graphql.schema,
       tempFile.path,
@@ -37,14 +40,57 @@ test('works', async () => {
       }
     );
 
-    const generatedContent = await readFile(destinationPath, {
+    const secondStats = await fs.promises.stat(tempFile.path);
+
+    expect(secondStats.mtimeMs).toBeGreaterThan(firstStats.mtimeMs);
+
+    // If the code didn't change, it shouldn't write anything
+    await writeGenerate(server.graphql.schema, tempFile.path, {
+      preImport: shouldBeIncluded,
+    });
+
+    const thirdStats = await fs.promises.stat(tempFile.path);
+
+    expect(secondStats.mtimeMs).toBe(thirdStats.mtimeMs);
+
+    const generatedContent = await fs.promises.readFile(destinationPath, {
       encoding: 'utf-8',
     });
 
     expect(generatedContent.startsWith(shouldBeIncluded)).toBeTruthy();
 
-    expect(generatedContent).toMatchSnapshot('basic');
+    expect(generatedContent).toMatchSnapshot('overwrite');
   } finally {
     await tempFile.cleanup();
+  }
+});
+
+test('creates dir, generates code and writes new file', async () => {
+  const tempDir = await tmp.dir({
+    unsafeCleanup: true,
+  });
+
+  try {
+    const targetPath = path.join(tempDir.path, '/new_path/file-to-generate.ts');
+
+    const shouldBeIncluded = '// This should be included';
+
+    const destinationPath = await writeGenerate(
+      server.graphql.schema,
+      targetPath,
+      {
+        preImport: shouldBeIncluded,
+      }
+    );
+
+    const generatedContent = await fs.promises.readFile(destinationPath, {
+      encoding: 'utf-8',
+    });
+
+    expect(generatedContent.startsWith(shouldBeIncluded)).toBeTruthy();
+
+    expect(generatedContent).toMatchSnapshot('new');
+  } finally {
+    await tempDir.cleanup();
   }
 });
