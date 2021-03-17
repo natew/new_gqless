dc97eb1330fa58853e5741a7036133126f1986fb
PabloSzx <pablosaez1995@gmail.com>
2021-03-17 14:04:53 -0300

    experiment using rollup for bunde & swc for testing

--- internal/test-utils/jest.config.js
+++ internal/test-utils/jest.config.js
@@ -2,8 +2,15 @@
  * @type {import("@jest/types").Config.InitialOptions}
  */
 module.exports = {
-  globals: {
-    'ts-jest': {},
+  transform: {
+    '^.+\\.tsx?$': [
+      '@swc-node/jest',
+      {
+        sourcemap: 'inline',
+        target: 'es2020',
+        module: 'commonjs',
+      },
+    ],
   },
   testMatch: ['**/test/**/*.test.ts', '**/test/**/*.test.tsx'],
   testPathIgnorePatterns: ['/node_modules/', '/test/generated'],
